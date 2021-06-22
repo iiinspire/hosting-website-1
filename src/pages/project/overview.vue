8be055d06c07594fb2f5834fd4a75aaecf80c2af
saullary <saullarytzc94@gmail.com>
2021-06-22 14:22:03 +0800

    chore: project overview btn ui

--- src/pages/project/overview.vue
+++ src/pages/project/overview.vue
@@ -12,9 +12,9 @@
 			<div class="y-center w100p d-flex al-c pd-20">
 				<div class="white-0">
 					<div class="fz-18">demo-app</div>
-					<div class="mt-1">
+					<div class="mt-1 d-flex al-c">
 						<v-icon color="#fff">mdi-github</v-icon>
-						<span class="op-5">demo/demo-app</span>
+						<span class="op-5 ml-1">demo/demo-app</span>
 					</div>
 				</div>
 				<div class="ml-auto">
@@ -23,10 +23,16 @@
 			</div>
 		</div>
 		<div class="pd-20">
-			<div class="d-flex al-c">
-				<div>
+			<div class="d-flex al-c flex-wrap">
+				<div class="mr-auto">
 					<div class="fz-18">Production Deploymen</div>
 					<div class="fz-13 mt-1 gray">The deployment that is available to your visitors</div>
+				</div>
+				<div class="d-flex al-c" :class="{
+					'mt-4': $vuetify.breakpoint.mobile,
+				}">
+					<v-btn small class="mr-5">View Build Logs</v-btn>
+					<v-btn small>View Domains</v-btn>
 				</div>
 			</div>
 		</div>
