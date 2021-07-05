b90a42d6e369cd3f314075252d8a36e33ab9530c
saullary <saullarytzc94@gmail.com>
2021-07-05 12:40:03 +0800

    chore: project st-domains

--- src/components/settings/st-domains.vue
+++ src/components/settings/st-domains.vue
@@ -1,5 +1,33 @@
 <template>
 <div>
-	domains
+	<h3>Domains</h3>
+	<div class="gray mt-1 fz-14">
+		These domains are assigned to your Production Deployments.Optionally, a different Git branch or a redirection to another domain can be configured for each one.
+	</div>
+	<div class="mt-5 d-flex">
+		<v-text-field outlined dense v-model="form.website" placeholder="mywebsite.com">
+		</v-text-field>
+		<v-btn color="primary" class="ml-4" style="margin-top: 2px;">Save</v-btn>
+	</div>
+	<div class="bd-1 mt-1">
+		<div class="pd-20">
+			<div class="d-flex al-c">
+				<h3>4everland.com</h3>
+				<v-chip small color="success" class="ml-3">production</v-chip>
+			</div>
+		</div>
+	</div>
 </div>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			form: {
+				website: '',
+			}
+		}
+	}
+}
+</script>
